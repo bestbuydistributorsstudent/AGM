f8c5c9bf52741c01bad2e0ca2a892413c3d83b93
bestbuydistributorsstudent <a.robinson@bestbuydistributors.ca>
2026-03-17 17:31:24 -0400

    Maybe

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import csp from "vite-plugin-csp-guard";
 
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 
@@ -14,6 +15,16 @@ const config = defineConfig({
     tailwindcss(),
     tanstackStart(),
     viteReact(),
+    csp({
+      algorithm: "sha256",
+      dev: {
+        run: true, 
+      },
+      policy: {
+        "script-src": ["'self'", "https://www.google-analytics.com"],
+        "style-src": ["'self'", "https://fonts.googleapis.com"], 
+      },
+    }),
   ],
 })
 
