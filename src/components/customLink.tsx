61938d25b6cfb3bf718bcbac08193cc7d9229390
bestbuydistributorsstudent <a.robinson@bestbuydistributors.ca>
2026-03-17 18:15:15 -0400

    idk

--- src/components/customLink.tsx
+++ src/components/customLink.tsx
@@ -1,26 +1,36 @@
-import {createLink, type LinkComponent} from "@tanstack/react-router";
+import { createLink, type LinkComponent } from "@tanstack/react-router";
+import type { AnchorHTMLAttributes } from "react";
 
-type CustomLinkComponentProps = React.AnchorHTMLAttributes<HTMLAnchorElement>&{
-    underline?:boolean;
+type CustomLinkComponentProps = AnchorHTMLAttributes<HTMLAnchorElement> & {
+  underline?: boolean;
 };
 
-export const CustomLinkComponent = (props: CustomLinkComponentProps) => {
-    return (<a {...props} className={`text-white p-2 ${props.underline && "bg-[#c30112] rounded-lg "}`}/>);
+export const CustomLinkComponent = ({
+  underline,
+  className,
+  ...rest
+}: CustomLinkComponentProps) => {
+  return (
+    <a
+      {...rest}
+      className={`text-white p-2 ${
+        underline ? "bg-[#c30112] rounded-lg" : ""
+      } ${className ?? ""}`}
+    />
+  );
 };
 
 export const TanStackLinkComp = createLink(CustomLinkComponent);
 
-export const CustomLink: LinkComponent<typeof TanStackLinkComp> = (
-    props
-) => {
-    return (
-        <TanStackLinkComp 
-        preload={"intent"}
-        activeProps={{
-            style: {color: "white"}, 
-             underline:true,
-        }}
-        {...props}
-        />
-    );
+export const CustomLink: LinkComponent<typeof TanStackLinkComp> = (props) => {
+  return (
+    <TanStackLinkComp
+      preload="intent"
+      activeProps={{
+        style: { color: "white" },
+        underline: true,
+      }}
+      {...props}
+    />
+  );
 };
